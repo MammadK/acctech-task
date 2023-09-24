ef37ddf0ec01619c72be6df793c8f3e7cd741872
Mammad Kazemi <rahadroid98@gmail.com>
2023-09-24 11:08:05 +0330

    add `fa` vocab for supporting persian i18n

---
+++ src/app/modules/i18n/vocabs/fa.ts
@@ -0,0 +1,131 @@
+// IRAN
+export const locale = {
+  lang: 'fa',
+  data: {
+    TRANSLATOR: {
+      SELECT: 'زبان مورد نظر را انتخاب کنید',
+    },
+    MENU: {
+      NEW: 'جدید', 
+      ACTIONS: 'عملیات', 
+      CREATE_POST: 'ایجاد پست جدید', 
+      PAGES: 'صفحات', 
+      FEATURES: 'امکانات', 
+      APPS: 'برنامه ها', 
+      DASHBOARD: 'پیشخوان', 
+      LAYOUT_BUILDER: 'صفحه‌آرایی',
+      PROFILE: 'نمایه',
+      OVERVIEW: 'بررسی',
+      PROJECTS: 'پروژه‌ها',
+      CAMPAIGNS: 'پویش‌ها',
+      DOCUMENTS: 'اسناد',
+      CONNECTIONS: 'ارتباطات',
+      WIZARDS: 'Wizards',
+      HORIZONTAL: 'افقی',
+      VERTICAL: 'عمودی',
+      ACCOUNTS: 'حساب‌هاِ کاربری',
+      SETTINGS: 'تنظیمات',
+      ERRORS: 'خطاها',
+      ERROR_404: 'خطای 404',
+      ERROR_500: 'خطای 500',
+      WIDGETS: 'ابزارک‌ها',
+      LISTS: 'لیست‌ها',
+      STATISTICS: 'آمار',
+      CHARTS: 'نمودارها',
+      MIXED: 'مختلط',
+      TABLES: 'جداول',
+      FEEDS: 'خوراک‌های خبری',
+      CHAT: 'چت',
+      PRIVATE_CHAT: 'چت خصوصی',
+      GROUP_CHART: 'چت گروهی',
+      DRAWER_CHART: 'نمودار در کشو'
+    },
+    AUTH: {
+      GENERAL: {
+        OR: 'یا', 
+        SUBMIT_BUTTON: 'ثبت', 
+        NO_ACCOUNT: 'حساب کاربری ندارید؟', 
+        SIGNUP_BUTTON: 'ثبت‌نام', 
+        FORGOT_BUTTON: 'کلمه عبور را فراموش کرده‌ام', 
+        BACK_BUTTON: 'بازگشت', 
+        PRIVACY: 'حریم خصوصی', 
+        LEGAL: 'حقوقی', 
+        CONTACT: 'ارتباط', 
+      },
+      LOGIN: {
+        TITLE: 'ورود به حساب کابری', 
+        BUTTON: 'ورود', 
+      },
+      FORGOT: {
+        TITLE: 'کلمه عبور را فراموش کرده اید؟', 
+        DESC: 'ایمیل خود را برای بازنشانی کلمه عبور وارد کنید.', 
+        SUCCESS: 'حساب کاربری شما با موفقیت بازنشانی شد.', 
+      },
+      REGISTER: {
+        TITLE: 'ثبت نام', 
+        DESC: 'مشخصات خود را برای ساخت اکانت وارد کنید.', 
+        SUCCESS: 'حساب کاربری شما با موفقیت ثبت شد.', 
+      },
+      INPUT: {
+        EMAIL: 'ایمیل', 
+        FULLNAME: 'نام و نام خانوادگی', 
+        PASSWORD: 'رمز عبور', 
+        CONFIRM_PASSWORD: 'تأیید رمز عبور', 
+        USERNAME: 'نام کاربری', 
+      },
+      VALIDATION: {
+        INVALID: '{{name}} معتبر نیست', 
+        REQUIRED: '{{name}} الزامی است', 
+        MIN_LENGTH: '{{name}} حداقل طول {{min}} دارد', 
+        AGREEMENT_REQUIRED: 'پذیرش شرایط و مقررات الزامی است', 
+        NOT_FOUND: '{{name}} درخواستی یافت نشد', 
+        INVALID_LOGIN: 'جزئیات ورود نادرست است', 
+        REQUIRED_FIELD: 'فیلد الزامی است', 
+        MIN_LENGTH_FIELD: 'حداقل طول فیلد:', 
+        MAX_LENGTH_FIELD: 'حداکثر طول فیلد:', 
+        INVALID_FIELD: 'فیلد معتبر نیست', 
+      }
+    },
+    ECOMMERCE: {
+      COMMON: {
+        SELECTED_RECORDS_COUNT: 'تعداد رکوردهای انتخاب‌شده: ', 
+        ALL: 'همه', 
+        SUSPENDED: 'متوقف‌شده', 
+        ACTIVE: 'فعال', 
+        FILTER: 'فیلتر', 
+        BY_STATUS: 'بر اساس وضعیت', 
+        BY_TYPE: 'بر اساس نوع', 
+        BUSINESS: 'کسب و کار', 
+        INDIVIDUAL: 'فردی', 
+        SEARCH: 'جستجو', 
+        IN_ALL_FIELDS: 'در تمام فیلدها', 
+      },
+      ECOMMERCE: 'تجارت الکترونیک', 
+      CUSTOMERS: {
+        CUSTOMERS: 'مشتریان', 
+        CUSTOMERS_LIST: 'لیست مشتریان', 
+        NEW_CUSTOMER: 'مشتری جدید', 
+        DELETE_CUSTOMER_SIMPLE: {
+          TITLE: 'حذف مشتری', 
+          DESCRIPTION: 'آیا از حذف دائمی این مشتری اطمینان دارید؟', 
+          WAIT_DESCRIPTION: 'مشتری در حال حذف شدن است...', 
+          MESSAGE: 'مشتری با موفقیت حذف شد', 
+        },
+        DELETE_CUSTOMER_MULTY: {
+          TITLE: 'حذف مشتریان', 
+          DESCRIPTION: 'آیا از حذف دائمی مشتریان انتخاب‌شده اطمینان دارید؟', 
+          WAIT_DESCRIPTION: 'مشتریان در حال حذف شدن هستند...', 
+          MESSAGE: 'مشتریان انتخابی با موفقیت حذف شدند', 
+        },
+        UPDATE_STATUS: {
+          TITLE: 'وضعیت مشتریان انتخابی به‌روز شد', 
+          MESSAGE: 'وضعیت مشتریان انتخابی با موفقیت به‌روز شد', 
+        },
+        EDIT: {
+          UPDATE_MESSAGE: 'مشتری به‌روز شد', 
+          ADD_MESSAGE: 'مشتری با موفقیت ایجاد شد', 
+        }
+      }
+    }
+  }
+};
